7b1fcca0b0f66de65273fcbb8e1d23aa5c31ff8f
Meghan Palagyi <meghanp@stanford.edu>
2013-04-11 11:54:57 -0700

    General slideshow.

--- js/gsb_feature_slideshow_fpp.flexslider.js
+++ js/gsb_feature_slideshow_fpp.flexslider.js
@@ -4,6 +4,10 @@ Drupal.behaviors.gsb_feature_slideshow_fpp_flexslider = {
   attach: function (context, settings) {
     $('.slideshow-wrapper').flexslider({
       animation: 'slide',
+      pauseOnHover: true,
+      touch: true,
+      slideshowSpeed: 10000,
+      animationSpeed: 600,
       selector: '.slideshow-items > li'
     });
   }
--- templates/slideshow-slide.tpl.php
+++ templates/slideshow-slide.tpl.php
@@ -1,11 +1,10 @@
 <div class="<?php print $classes; ?>">
-  <div class="slider-content">
-    <?php print render($media); ?>
-
-    <?php print render($title); ?>
-
-    <?php print render($description); ?>
-
-    <?php print render($cta_text); ?>
+  <?php print render($media); ?>
+  <div class="slide-text">
+    <div class="inner">
+      <?php print render($title); ?>
+      <?php print render($description); ?>
+      <?php print render($cta_text); ?>
+    </div>
   </div>
 </div>
