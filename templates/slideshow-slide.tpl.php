dbfca53880069661e3f4e0e9e9bcd179eed74c30
Tim Plunkett <git@plnktt.com>
2013-04-18 12:32:05 -0700

    Wrap the slideshow in a link.

--- templates/slideshow-slide.tpl.php
+++ templates/slideshow-slide.tpl.php
@@ -1,10 +1,1 @@
-<div class="<?php print $classes; ?>">
-  <?php print render($media); ?>
-  <div class="slide-text">
-    <div class="inner">
-      <?php print render($title); ?>
-      <?php print render($description); ?>
-      <?php print render($cta_text); ?>
-    </div>
-  </div>
-</div>
+<?php print render($slide); ?>
