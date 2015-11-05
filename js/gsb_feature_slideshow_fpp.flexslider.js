e34f62ee8300debce657b067a755f9dec69c73ce
Meghan Plunkett <meghanp@stanford.edu>
2015-11-05 18:03:13 -0500

    WP-3925: update the slideshow settings.

--- js/gsb_feature_slideshow_fpp.flexslider.js
+++ js/gsb_feature_slideshow_fpp.flexslider.js
@@ -3,10 +3,11 @@
 Drupal.behaviors.gsb_feature_slideshow_fpp_flexslider = {
   attach: function (context, settings) {
     $('.slideshow-wrapper').flexslider({
-      selector: ".slideshow-items > li",
-      animation: "slide",
-      slideshow: false,
+      selector: '.slideshow-items > li',
+      animation: 'slide',
+      animationLoop: true,
       animationSpeed: 600,
+      slideshow: false,
 
       // Usability features
       pauseOnHover: true,
@@ -22,7 +23,7 @@ Drupal.behaviors.gsb_feature_slideshow_fpp_flexslider = {
       keyboard: true,
 
       // Special properties
-      controlsContainer: ".slide-text-wrapper",
+      controlsContainer: '.slide-text-wrapper',
     });
   }
 };
